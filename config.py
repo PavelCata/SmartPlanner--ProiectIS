0ef08f54dd9d6f6544b9a3ae35b042b5242f853b
PavelCata <catalinpavel5@gmail.com>
2025-11-20 00:37:30 +0200

    resolved conflict

--- config.py
+++ config.py
@@ -1,8 +1,4 @@
 class Config:
     SECRET_KEY = "cheie-super-secreta"
     SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:PriPjtJXaqEoaCKNgFsYJOLfAuhyRejJ@nozomi.proxy.rlwy.net:17751/railway"
-<<<<<<< HEAD
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-=======
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
->>>>>>> origin/malan

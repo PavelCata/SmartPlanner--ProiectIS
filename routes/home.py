fcfb28dcfdefa6fa593ed04bb142ba3d40aad678
PavelCata <catalinpavel5@gmail.com>
2025-11-20 13:37:47 +0200

    catalin

--- routes/home.py
+++ routes/home.py
@@ -1,12 +1,24 @@
 from flask import Blueprint, render_template, redirect, url_for
-from flask_login import current_user
+from flask_login import current_user, login_required
+from models import Task
+from datetime import datetime, date, time
 
 home_bp = Blueprint("home", __name__)
 
 @home_bp.route("/")
+@login_required
 def index():
-    if not current_user.is_authenticated:
-        return redirect("/login")
-    return redirect(url_for("tasks.view_tasks"))
+    today = date.today()
+    now = datetime.now().time()
+    tasks_today = Task.query.filter_by(user_id=current_user.id, date=today).order_by(Task.start_time).all()
+    overdue_t = Task.query.filter(Task.user_id == current_user.id, Task.date < today).all()
+
+    next_task = None
+    for t in tasks_today:
+        if t.start_time > now:
+            next_task = t
+            break
+    
+    return render_template("index.html", tasks_today=tasks_today,overdue_tasks=overdue_t, next_task=next_task, selected_date=today, tasks = tasks_today)
 
  

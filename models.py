7069d1f3923ddfebd5a7a74463ca49ac529dda5e
PavelCata <catalinpavel5@gmail.com>
2025-12-11 11:16:20 +0200

    proxycata

--- models.py
+++ models.py
@@ -7,6 +7,7 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
     role = db.Column(db.String(10), nullable=False, default="user")
+    restricted = db.Column(db.Boolean, default=False)
 
     tasks = db.relationship("Task", backref="user", lazy=True, cascade="all,delete")
 
